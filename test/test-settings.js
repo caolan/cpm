cbdf47417be9ce194f813c608f30a6e73dbb5558
Caolan McMahon <caolan@caolanmcmahon.com>
2010-08-14 10:53:00 +0100

    added settings.validate

--- test/test-settings.js
+++ test/test-settings.js
@@ -22,7 +22,7 @@ exports['readSettingsFile'] = function (test) {
 };
 
 exports['loadSettings with local .cpmrc'] = function (test) {
-    test.expect(5);
+    test.expect(6);
     var _home = process.env['HOME'];
     process.env['HOME'] = '/home/user';
 
@@ -48,17 +48,23 @@ exports['loadSettings with local .cpmrc'] = function (test) {
         };
     };
 
-    settings.loadSettings('projectdir', function (err, settings) {
-        test.same(settings, {a:1,b:3,c:4});
+    var _validate = settings.validate;
+    settings.validate = function (settings) {
+        test.ok(true, 'validate called');
+    };
+
+    settings.loadSettings('projectdir', function (err, s) {
+        test.same(s, {a:1,b:3,c:4});
         process.env['HOME'] = _home;
         settings.readSettingsFile = _readSettingsFile;
+        settings.validate = _validate;
         path.exists = _exists;
         test.done();
     });
 };
 
 exports['loadSettings without local .cpmrc'] = function (test) {
-    test.expect(4);
+    test.expect(5);
     var _home = process.env['HOME'];
     process.env['HOME'] = '/home/user';
 
@@ -87,10 +93,16 @@ exports['loadSettings without local .cpmrc'] = function (test) {
         };
     };
 
-    settings.loadSettings('projectdir', function (err, settings) {
-        test.same(settings, {a:1,b:2});
+    var _validate = settings.validate;
+    settings.validate = function (settings) {
+        test.ok(true, 'validate called');
+    };
+
+    settings.loadSettings('projectdir', function (err, s) {
+        test.same(s, {a:1,b:2});
         process.env['HOME'] = _home;
         settings.readSettingsFile = _readSettingsFile;
+        settings.validate = _validate;
         path.exists = _exists;
         test.done();
     });
@@ -117,11 +129,64 @@ exports['loadSettings return errors from readSettingsFile'] = function (test) {
         });
     };
 
-    settings.loadSettings('projectdir', function (err, settings) {
+    var _validate = settings.validate;
+    settings.validate = function (settings) {
+        test.ok(false, 'dont validate files not read');
+    };
+
+    settings.loadSettings('projectdir', function (err, s) {
         test.equals(err, 'error');
         process.env['HOME'] = _home;
         settings.readSettingsFile = _readSettingsFile;
+        settings.validate = _validate;
         path.exists = _exists;
         test.done();
     });
+};
+
+exports['loadSettings return errors from validate'] = function (test) {
+    test.expect(3);
+    var _home = process.env['HOME'];
+    process.env['HOME'] = '/home/user';
+
+    var _exists = path.exists;
+    path.exists = function (p, callback) {
+        test.ok(true, 'path.exists called');
+        process.nextTick(function () {
+            callback(true);
+        });
+    };
+
+    var _readSettingsFile = settings.readSettingsFile;
+    settings.readSettingsFile = function (p, callback) {
+        callback();
+    };
+
+    var _validate = settings.validate;
+    settings.validate = function (settings) {
+        throw new Error('validation error');
+    };
+
+    settings.loadSettings('projectdir', function (err, s) {
+        test.equals(err.message, 'validation error');
+        process.env['HOME'] = _home;
+        settings.readSettingsFile = _readSettingsFile;
+        settings.validate = _validate;
+        path.exists = _exists;
+        test.done();
+    });
+};
+
+exports['validate ok'] = function (test) {
+    settings.validate({
+        cache: '/home/user/.cpm/cache'
+    });
+    test.done();
+};
+
+exports['validate - empty'] = function (test) {
+    test.expect(1);
+    try       { settings.validate({}); }
+    catch (e) { test.ok(e); }
+    test.done();
 };
