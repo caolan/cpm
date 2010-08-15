341c86d2fe109fc608c3584c453199ec84ce00ee
Caolan McMahon <caolan@caolanmcmahon.com>
2010-08-15 22:08:27 +0100

    change loadPackage to expect folder instead of package.json file path

--- test/test-packages.js
+++ test/test-packages.js
@@ -1,11 +1,23 @@
 var packages = require('../lib/packages');
 
+
 exports['loadPackage'] = function (test) {
-    var file = __dirname + '/fixtures/package.json';
-    packages.loadPackage(file, function(err, data){
-        test.same(data, [{_design: {
-            'package': {name: 'testpackage'}
-        }}]);
+    var file = __dirname + '/fixtures/testpackage';
+
+    packages.loadPackage(file, function (err, pkg, _design, attachments) {
+        test.same(pkg, {
+            name: 'testpackage',
+            dependencies: [
+                'testlib1',
+                'testlib2'
+            ],
+            directories: {
+                attachments: ["static"],
+                properties: ["views", "validate_doc_update.js"]
+            }
+        });
+        test.same(_design, {'package': pkg});
+        test.same(attachments, []);
         test.done();
     });
 };
