5fba61f9363ccb1a40996cdf261b4d4e7670cb48
Caolan McMahon <caolan@caolanmcmahon.com>
2010-08-15 22:09:12 +0100

    update setPropertyPath to use propertyName for final node name, stripping .js from the basename

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,9 +2,9 @@ var utils = require('../lib/utils');
 
 exports['propertyPath'] = function (test) {
     var obj = {test: 'test'};
-    utils.setPropertyPath(obj, 'some/example/path', 'val');
+    utils.setPropertyPath(obj, 'some/example/path.js', 'val');
     test.same(obj, {test: 'test', some: {example: {path: 'val'}}});
-    utils.setPropertyPath(obj, 'some/other/path', 'val2');
+    utils.setPropertyPath(obj, 'some/other/path.js', 'val2');
     test.same(obj, {
         test: 'test',
         some: {
