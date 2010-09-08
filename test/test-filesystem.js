d154ca1465770b9ae940f566df96f8a715f318a1
Caolan McMahon <caolan@caolanmcmahon.com>
2010-09-08 19:36:06 +0100

    update getPackage assertions to handle loading of files in different orders

--- test/test-filesystem.js
+++ test/test-filesystem.js
@@ -8,7 +8,28 @@ exports['getPackage - no dependencies'] = function (test) {
 
     filesystem.getPackage(settings, p, function (err, pkg) {
         if (err) throw err;
-        test.same(pkg, require(__dirname + '/fixtures/testpackage_loaded'));
+        var expected = require(__dirname + '/fixtures/testpackage_loaded');
+
+        test.same(Object.keys(pkg).sort(), Object.keys(expected).sort());
+
+        test.same(
+            Object.keys(pkg.cpm).sort(),
+            Object.keys(expected.cpm).sort()
+        );
+        test.same(pkg.cpm.files.sort(), expected.cpm.files.sort());
+        test.same(pkg.cpm.properties_files, expected.cpm.properties_files);
+
+        test.same(pkg._attachments, expected._attachments);
+        test.same(pkg.validate_doc_update, expected.validate_doc_update);
+        test.same(pkg.shows, expected.shows);
+        test.same(pkg.views, expected.views);
+        test.same(pkg.lib, expected.lib);
+        test.same(pkg.templates, expected.templates);
+        test.same(pkg.package, expected.package);
+        test.same(pkg.language, expected.language);
+        test.same(pkg.lists, expected.lists);
+        test.same(pkg.updates, expected.updates);
+        test.same(pkg.rewrites.sort(), expected.rewrites.sort());
         test.done();
     });
 };
